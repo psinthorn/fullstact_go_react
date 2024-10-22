1c24041d9859bd9284c83cff4d274b86fb7807d3
F2x <f2coltd@F2s-iMac.local>
2024-10-22 12:34:53 +0700

    update MongoDB Connecttion

--- logger-service/README.md
+++ logger-service/README.md
@@ -6,6 +6,7 @@
 
 1. install http framework in this app we use go-chi
 2. use mongodb as database
+   2.1 install mongodb driver
 
 1.1 Install http framework
 
@@ -15,5 +16,17 @@ go get github.com/go-chi/chi/v5
 go get github.com/go-chi/chi/v5/middleware
 <br>
 go get github.com/go-chi/cors
+
+```
+
+2. Install MongoDB driver and dependencies
+
+## install MongoDB driver
+
+```
+go get go.mongodb.org/mongo-driver/mongo
+<br>
+go get go.mongodb.org/mongo-driver/mongo/options
+<br>
 
 ```
--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/gommon/log"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// set up server serivce port
+const (
+	webPort  = "80"
+	mongoURL = "mongodb://mongo:27017"
+	rpcPort  = "5001"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
 
 type Config struct {
 	DB *sql.DB
@@ -16,23 +30,53 @@ func main() {
 
 	app := Config{}
 
-	// fmt.Println("Logger service is starting from now on... ;)")
-
 	// connect to database
-
-	// set up http server
-	const serverPort = "80"
-
-	log.Printf("Logger service is staring on port: %s", serverPort)
-
-	server := &http.Server{
-		Addr:    fmt.Sprintf(":%s", serverPort),
-		Handler: app.routes(),
-	}
-
-	err := server.ListenAndServe()
+	client, err := connectToMongoDB()
 	if err != nil {
 		log.Panic(err)
 	}
 
+	// create context in case of disconnect
+	ctx, cancle := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancle()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// http server start from here
+	log.Printf("Logger service is staring on port: %s", webPort)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
+
+func connectToMongoDB() (*mongo.Client, error) {
+
+	// create MongoDB clients options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "adminx",
+		Password: "adminx",
+	})
+
+	// create connection
+	conn, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Print("can't connect to MongoDB error: %s", err)
+		return nil, err
+	}
+
+	return conn, nil
 }
